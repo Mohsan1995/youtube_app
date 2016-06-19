b43e9ad9ac8969789a0ab4ff7dcdf347179211b2
Mohsan1995 <mohsan.dev@gmail.com>
2016-06-19 19:47:33 +0200

    add web service

--- app/src/main/java/niya/mohsan/youtube/MainActivity.java
+++ app/src/main/java/niya/mohsan/youtube/MainActivity.java
@@ -2,10 +2,19 @@ package niya.mohsan.youtube;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.TextView;
+
+import java.util.List;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
+import niya.mohsan.youtube.model.Youtube;
+import niya.mohsan.youtube.network.YoutubeProvider;
+import niya.mohsan.youtube.network.YoutubeService;
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -19,5 +28,21 @@ public class MainActivity extends AppCompatActivity {
 
         ButterKnife.bind(this);
         textView.setText("JZIJAIJDIAZ");
+
+        YoutubeService youtubeService = YoutubeProvider.get();
+
+        Call<List<Youtube>> call = youtubeService.listYoutube();
+
+        call.enqueue(new Callback<List<Youtube>>() {
+            @Override
+            public void onResponse(Call<List<Youtube>> call, Response<List<Youtube>> response) {
+                Log.e("MainActivity", response.body().get(0).getName());
+            }
+
+            @Override
+            public void onFailure(Call<List<Youtube>> call, Throwable t) {
+
+            }
+        });
     }
 }
--- app/src/main/java/niya/mohsan/youtube/network/YoutubeService.java
+++ app/src/main/java/niya/mohsan/youtube/network/YoutubeService.java
@@ -1,8 +1,19 @@
 package niya.mohsan.youtube.network;
 
+import java.util.List;
+
+import niya.mohsan.youtube.model.Youtube;
+import retrofit2.Call;
+import retrofit2.http.GET;
+
 /**
  * Created by mohsan on 19/06/16.
  */
-public class YoutubeService {
-    static String END_POINT = "https://raw.githubusercontent.com/florent37/MyYoutube/master/myyoutube.json";
+public interface YoutubeService {
+    static String END_POINT = "https://raw.githubusercontent.com/florent37/MyYoutube/master/";
+
+    @GET("myyoutube.json")
+    Call<List<Youtube>> listYoutube();
+
+
 }
